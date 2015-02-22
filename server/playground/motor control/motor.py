9d9475e8d6617d0692ba4eef50ffc2c68550202d
Jannis <jannis@boem.ch>
2015-02-22 21:47:10 +0100

    readme update

--- server/playground/motor control/motor.py
+++ server/playground/motor control/motor.py
@@ -3,33 +3,68 @@ import RPi.GPIO as gpio
 from time import sleep
 import sys
 
-blue = 12 #GPIO 18
-pink = 16 #GPIO 23
-yellow = 18 #GPIO 24
-orange = 22 #GPIO 25
+pin1 = 12 #GPIO 18
+pin2 = 16 #GPIO 23
+pin3 = 18 #GPIO 24
+pin4 = 22 #GPIO 25
 
 gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
-gpio.setup(blue, gpio.OUT)
-gpio.setup(pink, gpio.OUT)
-gpio.setup(yellow, gpio.OUT)
-gpio.setup(orange, gpio.OUT)
+gpio.setup(pin1, gpio.OUT)
+gpio.setup(pin2, gpio.OUT)
+gpio.setup(pin3, gpio.OUT)
+gpio.setup(pin4, gpio.OUT)
 
-StepValues = [
-    [1,1,0,0,0,0,0,1],
-    [0,1,1,1,0,0,0,0],
-    [0,0,0,1,1,1,0,0],
-    [0,0,0,0,0,1,1,1],
-]
+case = 1
 
 def turn():
-    for step in StepValues:
-        #print(step)
-        gpio.output(blue, step[0])
-        gpio.output(pink, step[1])
-        gpio.output(yellow, step[2])
-        gpio.output(orange, step[3])
-        sleep(0.01)
+    global case
+    if case == 1:
+        gpio.output(pin1, gpio.LOW)
+        gpio.output(pin2, gpio.LOW)
+        gpio.output(pin3, gpio.LOW)
+        gpio.output(pin4, gpio.HIGH)
+    if case == 2:
+        gpio.output(pin1, gpio.LOW)
+        gpio.output(pin2, gpio.LOW)
+        gpio.output(pin3, gpio.HIGH)
+        gpio.output(pin4, gpio.HIGH)
+    if case == 3:
+        gpio.output(pin1, gpio.LOW)
+        gpio.output(pin2, gpio.LOW)
+        gpio.output(pin3, gpio.HIGH)
+        gpio.output(pin4, gpio.LOW)
+    if case == 4:
+        gpio.output(pin1, gpio.LOW)
+        gpio.output(pin2, gpio.HIGH)
+        gpio.output(pin3, gpio.LOW)
+        gpio.output(pin4, gpio.LOW)
+    if case == 5:
+        gpio.output(pin1, gpio.HIGH)
+        gpio.output(pin2, gpio.HIGH)
+        gpio.output(pin3, gpio.LOW)
+        gpio.output(pin4, gpio.LOW)
+    if case == 6:
+        gpio.output(pin1, gpio.HIGH)
+        gpio.output(pin2, gpio.LOW)
+        gpio.output(pin3, gpio.LOW)
+        gpio.output(pin4, gpio.LOW)
+    if case == 7:
+        gpio.output(pin1, gpio.HIGH)
+        gpio.output(pin2, gpio.LOW)
+        gpio.output(pin3, gpio.LOW)
+        gpio.output(pin4, gpio.HIGH)
+    else:
+        gpio.output(pin1, gpio.LOW)
+        gpio.output(pin2, gpio.LOW)
+        gpio.output(pin3, gpio.LOW)
+        gpio.output(pin4, gpio.LOW)
+#    print case
+    case = case + 1
+    if case>7:
+        case = 1
+#        print 'LOG: One turn'
+    sleep(0.01)
 
 while True:
     turn()
