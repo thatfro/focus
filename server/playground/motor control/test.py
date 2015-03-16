348d7afb42f1620f1fa1e70fc9da2a3c93670367
Jannis <jannis@boem.ch>
2015-03-16 22:54:21 +0100

    motor control file modified

--- server/playground/motor control/test.py
+++ server/playground/motor control/test.py
@@ -5,24 +5,24 @@ import sys
 
 gpio.setmode(gpio.BOARD)
 gpio.setwarnings(False)
-enable_1 = 8
-motor_1 = 10
-motor_2 = 12
-enable_2 = 22
-motor_3 = 24
-motor_4 = 26
+enable_1 = 8 #GPIO 14
+motor_1 = 10 #GPIO 15
+motor_2 = 12 #GPIO 18
+enable_2 = 22 #GPIO 25
+motor_3 = 24 #GPIO 8
+motor_4 = 26 #GPIO 7
 
-gpio.setup(motor_1, gpio.out)
-gpio.setup(motor_2, gpio.out)
-gpio.setup(motor_3, gpio.out)
-gpio.setup(motor_4, gpio.out)
-gpio.setup(enable_1, gpio.out)
-gpio.setup(enable_2, gpio.out)
+gpio.setup(motor_1, gpio.OUT)
+gpio.setup(motor_2, gpio.OUT)
+gpio.setup(motor_3, gpio.OUT)
+gpio.setup(motor_4, gpio.OUT)
+gpio.setup(enable_1, gpio.OUT)
+gpio.setup(enable_2, gpio.OUT)
 
-StepValues =    [0,1,1,0] #Step 1
-                [0,1,0,1] #Step 2
-                [1,0,0,1] #Step 3
-                [1,0,1,0] #Step 4
+StepValues =   [[0,1,1,0], #Step 1
+                [0,1,0,1], #Step 2
+                [1,0,0,1], #Step 3
+                [1,0,1,0]] #Step 4
 
 def turn(f):
     if f == True:
@@ -30,8 +30,8 @@ def turn(f):
             # Loop through steps to turn
             gpio.output(motor_1, step[0])
             gpio.output(motor_2, step[1])
-            gpio.output(motor_3, step[3])
-            gpio.output(motor_4, step[4])
+            gpio.output(motor_3, step[2])
+            gpio.output(motor_4, step[3])
             sleep(0.01) # Delay between rotations
     if f == False:
         # Hold position
@@ -47,9 +47,8 @@ def main():
     gpio.output(enable_1, gpio.HIGH)
     gpio.output(enable_2, gpio.HIGH)
     # Perform infinite turns
-    try:
-        while True:
-                turn(True)
-    # Hold motor on interrupt
-    except KeyboardInterrupt:
-        turn(False)
+    while True:
+        print "Running!"
+        turn(True)
+
+    
